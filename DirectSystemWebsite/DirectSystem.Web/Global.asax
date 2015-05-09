869a1c4dc70c6f28545597493de2c4e913b78f6b
Tuan Nguyen <tuankun1211@gmail.com>
2015-05-09 09:08:07 +0700

    Re structure of project

---
+++ DirectSystemWebsite/DirectSystem.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DirectSystem.Web.MvcApplication" Language="C#" %>
--- DirectSystemWebsite/DirectSystemWebsite/Global.asax.cs
+++ DirectSystemWebsite/DirectSystemWebsite/Global.asax.cs
@@ -22,6 +22,7 @@ namespace DirectSystemWebsite
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
